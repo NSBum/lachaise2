e33b66a966fabde8c8715f548cfa4faecabde4d2
Buddha <duncan.alan@me.com>
2015-02-07 15:32:21 -0600

    Documentation, Note as struct

--- NoteManager.h
+++ NoteManager.h
@@ -4,25 +4,58 @@
 #include <FastLED.h>
 #include "Note.h"
 
+/**
+ *	A note type
+ *
+ *	Represents a visual "note" played upon the "keyboard" of the chair.
+ *
+ */
 typedef struct note_t {
-	uint16_t start;
-	uint16_t end;
-	uint8_t hue;
-	bool playing;
-}
+	uint16_t start;		/**< linear start location */
+	uint16_t end;		/**< linear end location */
+	uint8_t hue;		/**< hue of the block */
+	bool playing;		/**< true if playing, otherwise false */
+} note_t;
 
+/**	
+ *	Manages notes on the "keyboard"
+ */
 class NoteManager {
 public:
+	/** Constructor */
 	NoteManager();
+
+	/** Plays a note at the touch index
+	 *
+	 *	@param touchIndex The index of the touch sensor
+	 *	@note The touchIndex is mapped in reverse order to the LED segments
+	 */
 	static void play(uint8_t touchIndex);
+
+	/**	Releases a note
+	 *
+	 *	@param touchIndex The index of the touch sensor being released
+	 */
 	static void release(uint8_t touchIndex);
+
+	/**	Update the LED strands
+	 *
+	 *	@param leds The array of LEDs to update
+	 */
 	static void update(CRGB leds[]);
 
+	/**	Initialization of the notes
+	 *
+	 *	Must be called to initialize in setup()
+	 */
+	static void begin();
+
 private:
-	static const Note *_notes;
+	static note_t _notes[NUM_NOTES];
 	static uint8_t noteIndexForTouch(uint8_t touchIndex);
 };
 
+/** Singleton instance of the class */
 extern NoteManager Conductor;
 
 #endif
