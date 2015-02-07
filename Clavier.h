30380b12a49d21316e53e0b65b0e54559dd1c4b9
Buddha <duncan.alan@me.com>
2015-02-07 15:33:02 -0600

    Documentation

--- Clavier.h
+++ Clavier.h
@@ -6,15 +6,45 @@
 #include <Arduino.h>
 #include <Adafruit_MPR121.h>
 
-
-
+/**
+ * 	Represents a virtual keyboard
+ *
+ *	The programmatic interface to the capacitive touch sensor
+ */
 class Clavier {
 public:
+	/**	Constructor */
 	Clavier();
+
+	/** Begins the interaction with the MPR121 capacitive touch sensor
+	 *
+	 *	@return Returns true if the interaction begins correctly, false otherwise.
+	 */
 	static bool begin();
+
+	/**	Status of the touch sensor
+	 *
+	 *	Returns true if the sensor is online, false otherwise
+	 */
 	static bool sensorOnline();
+
+	/** Update the status of the sensors
+	 *
+	 *	*return Returns true if a sensor changed since the last update.
+	 *	@note This must be called with each cycle of the run loop.
+	 */
 	static bool update();
+
+	/**	New touches
+	 *
+	 *	Returns the new touches on the sensors as a bitfield.
+	 */
 	static uint16_t newTouches();
+
+	/** New releases
+	 *
+	 *	Returns the new releases on the sensors as a bitfield.
+	 */
 	static uint16_t newReleases();
 private:
 	static uint16_t currtouched;
@@ -26,6 +56,7 @@ private:
 	static bool sensorInterfaceOnline;
 };
 
+/**	Singleton instance */
 extern Clavier Klavier;
 
 #endif
