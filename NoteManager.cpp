f92727c453e164eae248fa34f9dd1cac9ac08488
Buddha <duncan.alan@me.com>
2015-02-08 17:43:05 -0600

    permit fading in and out

--- NoteManager.cpp
+++ NoteManager.cpp
@@ -5,18 +5,31 @@
 #undef DEBUG_NOTE_CREATION
 #undef DEBUG_NOTE_PLAYING
 
-note_t NoteManager::_notes[NUM_NOTES];
+note_vec_t NoteManager::_notes;
+bool NoteManager::_slowMode;
+bool NoteManager::_echo;
+echo_vec_t NoteManager::_echoes;
 
 NoteManager::NoteManager() {
 	
 }
 
 void NoteManager::begin() {
-	uint8_t hue_inc = 0;
-	if( NUM_NOTES != 0 )
-		hue_inc = 0xFF / NUM_NOTES;
-	else
-		hue_inc = 0x11;
+	//	make room for notes
+	_notes.reserve(7);
+
+	_slowMode = false;
+	_echo = false;
+
+	//	make room for an echo queue
+	_echoes.reserve(20);
+
+	uint8_t rainbow_hues[7] = {0,36,72,108,144,180,216};
+	// uint8_t hue_inc = 0;
+	// if( NUM_NOTES != 0 )
+	// 	hue_inc = 0xFF / NUM_NOTES;
+	// else
+	// 	hue_inc = 0x11;
 	for(uint8_t i = 0; i < NUM_NOTES; i++ ) {
 		uint8_t length = NUM_LEDS / NUM_NOTES;
 		uint16_t start = i * length;
@@ -27,11 +40,15 @@ void NoteManager::begin() {
 		note_t temp_note;
 		temp_note.start = start;
 		temp_note.end = end;
-		temp_note.hue = hue_inc * i;
+		temp_note.fade = 0;
+		temp_note.fadeDirection = FadeNone;
+		//temp_note.hue = hue_inc * i;
+		temp_note.color = CHSV(rainbow_hues[i],0xFF,0xFF);
 		temp_note.playing = false;
-		_notes[i] = temp_note;	
-		note_t n = _notes[i];
+		_notes.push_back(temp_note);	
+		
 		#ifdef DEBUG_NOTE_CREATION
+		note_t n = _notes[i];
 			//Serial.print("touch idx = "); Serial.println(touchIndex);
 			Serial.print("note idx = "); Serial.println(i);
 			Serial.print("range: "); Serial.print(n.start); Serial.print("->"); Serial.println(n.end);
@@ -42,18 +59,120 @@ void NoteManager::begin() {
 
 void NoteManager::update(CRGB leds[]) {
 	//	update our LEDs based on which notes are playing
-	for(uint8_t i = 0; i < NUM_NOTES; i++ ) {
-		note_t note = _notes[i];
-		if( note.playing ) {
-			uint16_t start = note.start;
-			uint16_t end = note.end;
-			uint8_t hue = note.hue;
-			uint8_t bright = 0xFF;
-			for(uint16_t i = start; i <= end; i++ ) {
-				leds[i] = CHSV(hue,255,255);
+	std::vector<note_t>::iterator it;
+	for(it = _notes.begin(); it != _notes.end(); ++it) {
+	//for(uint8_t i = 0; i < NUM_NOTES; i++ ) {
+		//note_t note = _notes[i];
+		uint16_t start = it->start;
+		uint16_t end = it->end;
+		if( it->playing ) {
+			
+
+			uint8_t bright;
+			if( !_slowMode ) {
+				bright = 0xFF;
+				for(uint16_t i = start; i <= end; i++ ) {
+					leds[i] = it->color;
+				}
+			}
+			else {
+				//	slow mode is on
+				if( it->fadeDirection == FadeIn ) {
+					//	time for an update?
+					uint32_t currentTime = millis();
+					if( 1 ) {
+						//Serial.print("Fade in: "); Serial.println(it->fade);
+						for(uint16_t i = start; i <= end; i++ ) {
+							leds[i] = it->color;//.fadeLightBy(255 - it->fade);
+							leds[i].fadeToBlackBy(255 - it->fade);
+						}
+						it->lastFadeTime = currentTime;
+						if( it->fade < 245 ) {
+							it->fade = it->fade + 10;
+							//Serial.print("increased note.fade to "); Serial.println(it->fade);
+						}
+						else {
+							//Serial.println("will note increase note.fade");
+						}
+					}
+					
+				}
+
+			}
+			//	if we are echoing, then we have some modifications to make
+		}
+		else if( it->fadeDirection == FadeOut ) {
+			//	time to update?
+			uint32_t currentTime = millis();
+			if( 1 ) {
+				for(uint16_t i = start; i <= end; i++ ) {
+					leds[i] = it->color;
+					leds[i].fadeToBlackBy(it->fade);
+					Serial.print("Fade out: "); Serial.println(it->fade);
+				}
+				it->lastFadeTime = currentTime;
+			}
+			if( it->fade <= 245 ) {
+				it->fade = it->fade + 10;
+			}
+			else {
+				it->fadeDirection == FadeNone;
 			}
 		}
 	}
+
+	//	process any echoes as needed
+	if( _echo ) {
+		std::vector<echo_t>::iterator it = _echoes.begin();
+		for(; it != _echoes.end(); ) {
+			uint32_t currentTime = millis();
+
+			//	time for a change?
+			if( currentTime - it->lastChangeTime >= 75 ) {
+				//	toggle the current state & record the time
+				it->currentState = !it->currentState;
+				it->lastChangeTime = currentTime;
+
+				//	do we need to "play" the echo?
+				if( it->currentState ) {
+					Serial.print("echo #"); Serial.println(it->echoCount);
+					it->spatialOffset -= 10;
+					it->echoCount++;
+					if( it->echoCount > 4 )
+						it->echoCount = 4;
+
+					//	what note are we referencing?
+					note_t note = _notes[it->noteIndex];
+					CRGB color = note.color;
+
+					//	dim it according to the echo count
+					uint8_t dims[5] = {30,64,128,192,230};
+					color = color.fadeToBlackBy(dims[it->echoCount]);
+
+					//	displace the echo, but don't go beyond the end
+					uint16_t start = note.start + it->spatialOffset;
+					if( start > NUM_LEDS ) 
+						start = NUM_LEDS;
+					uint16_t end = note.end + it->spatialOffset;
+					if( end > NUM_LEDS )
+						end = NUM_LEDS;
+					for(uint16_t i = start; i <= end; i++ ) {
+						leds[i] = color;
+					}
+				}
+			}
+			if( it->echoCount >= 4 ) {
+				it = _echoes.erase(it);
+			}
+			else {
+				it++;
+			}
+		}
+	}
+}
+
+void NoteManager::setColor(uint8_t noteIndex, CRGB color) {
+	_notes[noteIndex].color = color;
 }
 
 void NoteManager::play(uint8_t touchIndex) {
@@ -62,6 +181,27 @@ void NoteManager::play(uint8_t touchIndex) {
 	}
 	uint8_t noteIndex = noteIndexForTouch(touchIndex);
 	_notes[noteIndex].playing = true;
+
+	//	if we're in slow mode, then note has to fade in
+	if( _slowMode ) {
+		_notes[noteIndex].lastFadeTime = millis();
+		_notes[noteIndex].fadeDirection = FadeIn;
+		_notes[noteIndex].fade = 0;
+		Serial.println("Fade set to 0");
+	}
+
+	//	if _echo is on, we need to push a new echo
+	if( _echo ) {
+		echo_t e ;
+		e.noteIndex = noteIndex;
+		e.lastChangeTime = millis();
+		e.currentState = true;
+		e.spatialOffset = 0;
+		e.echoCount = 0;
+
+		//	push it onto the stack
+		_echoes.push_back(e);
+	}
 
 	#ifdef DEBUG_NOTE_PLAYING
 		note_t n = _notes[noteIndex];
@@ -79,6 +219,22 @@ void NoteManager::release(uint8_t touchIndex) {
 	}
 	uint8_t noteIndex = noteIndexForTouch(touchIndex);
 	_notes[noteIndex].playing = false;
+
+	if( _slowMode ) {
+		_notes[noteIndex].lastFadeTime = millis();
+		_notes[noteIndex].fadeDirection = FadeOut;
+		_notes[noteIndex].fade = 0;
+	}
+}
+
+void NoteManager::toggleSlowMode() {
+	_slowMode = !_slowMode;
+	if( _slowMode ) Serial.println("SLOWMODE -> ON");
+	else Serial.println("SLOWMODE -> OFF");
+}
+
+void NoteManager::toggleEcho() {
+	_echo = !_echo;
 }
 
 uint8_t NoteManager::noteIndexForTouch(uint8_t touchIndex) {
--- NoteManager.h
+++ NoteManager.h
@@ -2,7 +2,14 @@
 #define _CHAIR_AFFAIR_NOTE_MANAGER
 
 #include <FastLED.h>
+#include <vector>
 #define NUM_NOTES 7
+
+typedef enum {
+	FadeIn,
+	FadeOut,
+	FadeNone
+} fade_dir_t;
 
 /**
  *	A note type
@@ -13,9 +20,34 @@
 typedef struct note_t {
 	uint16_t start;		/**< linear start location */
 	uint16_t end;		/**< linear end location */
-	uint8_t hue;		/**< hue of the block */
 	bool playing;		/**< true if playing, otherwise false */
+	CRGB color;
+	uint8_t fade;		/**< fade if slow mode */
+	fade_dir_t fadeDirection;
+	uint32_t lastFadeTime;
 } note_t;
+
+typedef std::vector<note_t> note_vec_t;
+
+/**
+ *	Echo
+ *
+ *	After a note is touched, depending on the global echo setting,
+ *	it may generate an "echo". These echoes propagate down the 
+ *	LED string with diminishing brightness and length until they
+ *	completely fade out, or reach the end of the string.
+ *
+ */
+typedef struct echo_t {
+	uint8_t noteIndex;			/**< index of the keyboard note */
+	uint16_t spatialOffset;		/**< current linear delta */
+	bool currentState;			/**< true is ON, false is OFF */
+	uint32_t lastChangeTime;	/**< millisecond stamp at last state change */ 
+	uint8_t currentBrightness;	/**< brightness factor */
+	uint8_t echoCount;			/**< number of echoes displayed */
+} echo_t;
+
+typedef std::vector<echo_t> echo_vec_t;
 
 /**	
  *	Manages notes on the "keyboard"
@@ -44,6 +76,12 @@ public:
 	 */
 	static void update(CRGB leds[]);
 
+	static void setColor(uint8_t noteIndex, CRGB color);
+
+	static void toggleSlowMode();
+
+	static void toggleEcho();
+
 	/**	Initialization of the notes
 	 *
 	 *	Must be called to initialize in setup()
@@ -51,7 +89,10 @@ public:
 	static void begin();
 
 private:
-	static note_t _notes[NUM_NOTES];
+	static note_vec_t _notes;
+	static bool _slowMode;
+	static bool _echo;
+	static echo_vec_t _echoes;
 	static uint8_t noteIndexForTouch(uint8_t touchIndex);
 };
 
