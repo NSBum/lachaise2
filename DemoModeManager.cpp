717537c2b14bf48f9db80f6133a9ec2ca8a72fa0
Buddha <duncan.alan@me.com>
2015-02-10 23:36:01 -0600

    added variation A, integrate spectra

--- DemoModeManager.cpp
+++ DemoModeManager.cpp
@@ -1,5 +1,7 @@
 #include "DemoModeManager.h"
 
+uint8_t DemoModeManager::_songIndex;
+uint16_t DemoModeManager::_startIndex;
 bool DemoModeManager::_override;
 bool DemoModeManager::_state;
 uint16_t DemoModeManager::_noteIndex;
@@ -39,11 +41,64 @@ piano_roll_note_t twinkle_theme[] = {
 	{ .note = 99, .dur = 0}
 };
 
+piano_roll_note_t twinkle_A[] = {
+	{.note = 0, .dur = 200},{.note = 0, .dur = 200},{.note = 0, .dur = 200},{.note = 0, .dur = 200},
+	{.note = 0, .dur = 400},{.note = 0, .dur = 400},
+	{.note = 4, .dur = 200},{.note = 4, .dur = 200},{.note = 4, .dur = 200},{.note = 4, .dur = 200},
+	{.note = 4, .dur = 400},{.note = 4, .dur = 400},
+	{.note = 5, .dur = 200},{.note = 5, .dur = 200},{.note = 5, .dur = 200},{.note = 5, .dur = 200},
+	{.note = 5, .dur = 400},{.note = 5, .dur = 400},
+	{.note = 4, .dur = 200},{.note = 4, .dur = 200},{.note = 4, .dur = 200},{.note = 4, .dur = 200},
+	{.note = 4, .dur = 400},{.note = 4, .dur = 400},
+	{.note = 3, .dur = 200},{.note = 3, .dur = 200},{.note = 3, .dur = 200},{.note = 3, .dur = 200},
+	{.note = 3, .dur = 400},{.note = 3, .dur = 400},
+	{.note = 2, .dur = 200},{.note = 2, .dur = 200},{.note = 2, .dur = 200},{.note = 2, .dur = 200},
+	{.note = 2, .dur = 400},{.note = 2, .dur = 400},
+	{.note = 1, .dur = 200},{.note = 1, .dur = 200},{.note = 1, .dur = 200},{.note = 1, .dur = 200},
+	{.note = 1, .dur = 400},{.note = 1, .dur = 400},
+	{.note = 0, .dur = 200},{.note = 0, .dur = 200},{.note = 0, .dur = 200},{.note = 0, .dur = 200},
+	{.note = 0, .dur = 400},{.note = 0, .dur = 400},
+	{.note = 4, .dur = 200},{.note = 4, .dur = 200},{.note = 4, .dur = 200},{.note = 4, .dur = 200},
+	{.note = 4, .dur = 400},{.note = 4, .dur = 400},
+	{.note = 3, .dur = 200},{.note = 3, .dur = 200},{.note = 3, .dur = 200},{.note = 3, .dur = 200},
+	{.note = 3, .dur = 400},{.note = 3, .dur = 400},
+	{.note = 2, .dur = 200},{.note = 2, .dur = 200},{.note = 2, .dur = 200},{.note = 2, .dur = 200},
+	{.note = 2, .dur = 400},{.note = 2, .dur = 400},
+	{.note = 1, .dur = 200},{.note = 1, .dur = 200},{.note = 1, .dur = 200},{.note = 1, .dur = 200},
+	{.note = 1, .dur = 400},{.note = 1, .dur = 400},
+	{.note = 4, .dur = 200},{.note = 4, .dur = 200},{.note = 4, .dur = 200},{.note = 4, .dur = 200},
+	{.note = 4, .dur = 400},{.note = 4, .dur = 400},
+	{.note = 3, .dur = 200},{.note = 3, .dur = 200},{.note = 3, .dur = 200},{.note = 3, .dur = 200},
+	{.note = 3, .dur = 400},{.note = 3, .dur = 400},
+	{.note = 2, .dur = 200},{.note = 2, .dur = 200},{.note = 2, .dur = 200},{.note = 2, .dur = 200},
+	{.note = 2, .dur = 400},{.note = 2, .dur = 400},
+	{.note = 1, .dur = 200},{.note = 1, .dur = 200},{.note = 1, .dur = 200},{.note = 1, .dur = 200},
+	{.note = 1, .dur = 400},{.note = 1, .dur = 400},
+	{.note = 0, .dur = 200},{.note = 0, .dur = 200},{.note = 0, .dur = 200},{.note = 0, .dur = 200},
+	{.note = 0, .dur = 400},{.note = 0, .dur = 400},
+	{.note = 4, .dur = 200},{.note = 4, .dur = 200},{.note = 4, .dur = 200},{.note = 4, .dur = 200},
+	{.note = 4, .dur = 400},{.note = 4, .dur = 400},
+	{.note = 5, .dur = 200},{.note = 5, .dur = 200},{.note = 5, .dur = 200},{.note = 5, .dur = 200},
+	{.note = 5, .dur = 400},{.note = 5, .dur = 400},
+	{.note = 4, .dur = 200},{.note = 4, .dur = 200},{.note = 4, .dur = 200},{.note = 4, .dur = 200},
+	{.note = 4, .dur = 400},{.note = 4, .dur = 400},
+	{.note = 3, .dur = 200},{.note = 3, .dur = 200},{.note = 3, .dur = 200},{.note = 3, .dur = 200},
+	{.note = 3, .dur = 400},{.note = 3, .dur = 400},
+	{.note = 2, .dur = 200},{.note = 2, .dur = 200},{.note = 2, .dur = 200},{.note = 2, .dur = 200},
+	{.note = 2, .dur = 400},{.note = 2, .dur = 400},
+	{.note = 1, .dur = 200},{.note = 1, .dur = 200},{.note = 1, .dur = 200},{.note = 1, .dur = 200},
+	{.note = 1, .dur = 400},{.note = 1, .dur = 400},
+	{.note = 0, .dur = 200},{.note = 0, .dur = 200},{.note = 0, .dur = 200},{.note = 0, .dur = 200},
+	{.note = 0, .dur = 400},{.note = 0, .dur = 400},
+	{ .note = 99, .dur = 0}
+};
+
 DemoModeManager::DemoModeManager(int ledPin) {
 	_playingSong = false;
 	_state = false;
 	_override = true;
 	_led_pin = ledPin;
+	_startIndex = 0;
 	pinMode(_led_pin,OUTPUT);
 	digitalWrite(_led_pin,LOW);
 
@@ -74,7 +129,7 @@ void DemoModeManager::start() {
 	_noteIndex = 0;
 	_noteState = false;
 	_noteTimer = 0;
-	_playingSong = true;
+	_playingSong = false;
 	digitalWrite(_led_pin,HIGH);
 }
 
@@ -87,45 +142,103 @@ void DemoModeManager::stop() {
 	digitalWrite(_led_pin,LOW);
 }
 
-uint8_t DemoModeManager::update() {
+uint8_t DemoModeManager::update(CRGB leds[]) {
 	if( !_state | (_state & _override) ) { return DEMO_MODE_NO_NOTE; }
-	if( _noteState == false ) {
-		//	we're in a rest
-		if( millis() - _noteTimer > 100 ) {
-			//	//rest is over, play the note
-			piano_roll_note_t n = twinkle_theme[_noteIndex++];
-			if( n.note != 99 ) {
-				//	this is a valid note
-				//	start timer
-				_noteTimer = millis();
-				_noteState = true;
-				return n.note;
+	if( _playingSong ) {
+		if( _noteState == false ) {
+			//	we're in a rest
+			if( millis() - _noteTimer > 100 ) {
+				//	//rest is over, play the note
+				piano_roll_note_t n = noteFromSongWithIndex(_songIndex,_noteIndex);
+				_noteIndex++;
+				if( n.note != 99 ) {
+					//	this is a valid note
+					//	start timer
+					_noteTimer = millis();
+					_noteState = true;
+					return n.note;
+				}
+				else {
+					//	this is the end of the song
+					_noteIndex = 0;
+					_noteState = false;
+					_noteTimer = 0;
+					_playingSong = false;
+					return DEMO_MODE_NO_NOTE;
+				}
 			}
-			else {
-				//	this is the end of the song
-				_noteIndex = 0;
+		} // not playing a note
+		else {
+			//	we're in a note
+			piano_roll_note_t n = noteFromSongWithIndex(_songIndex,_noteIndex);
+			if( millis() - _noteTimer > n.dur ) {
+				//	note is over
+				_noteTimer = millis();
 				_noteState = false;
-				_noteTimer = 0;
-				_playingSong = false;
 				return DEMO_MODE_NO_NOTE;
 			}
-		}
-	}
+			else {
+				return n.note;
+			}
+		} // playing a note
+	} // playing a song
 	else {
-		//	we're in a note
-		piano_roll_note_t n = twinkle_theme[_noteIndex];
-		if( millis() - _noteTimer > n.dur ) {
-			//	note is over
-			_noteTimer = millis();
-			_noteState = false;
-			return DEMO_MODE_NO_NOTE;
+		//	
+		_startIndex++;
+		fillLEDsFromPaletteColors(leds,_startIndex);
+	} // *not* playing a song
+	changeDemoModePeriodically();
+	
+}
+
+piano_roll_note_t DemoModeManager::noteFromSongWithIndex(uint8_t aSongIndex,uint16_t aNoteIndex) {
+	if( aSongIndex == 0 ) {
+		return twinkle_theme[aNoteIndex];
+	}
+	else if( aSongIndex == 1 ) {
+		return twinkle_A[aNoteIndex];
+	}
+	piano_roll_note_t randomNote = {.note = random(0,6), .dur = random(250,200) };
+}
+
+void DemoModeManager::changeDemoModePeriodically() {
+	uint8_t minuteHand = (millis() / 60000 ) % 6;
+	static uint8_t lastMinute = 99;
+
+	DEBUG_PRINT("DEMO | new minute");
+	if( lastMinute != minuteHand ) {
+		lastMinute = minuteHand;
+		if( minuteHand == 0 ) {
+			DEBUG_PRINT("DEMO | rainbow blended palette");
+			currentPalette = RainbowColors_p;
+			currentBlending = BLEND;
+		}
+		else if( minuteHand == 2 ) {
+			DEBUG_PRINT("DEMO | rainbow stripe no blend");
+			currentPalette = RainbowStripeColors_p;
+			currentBlending = NOBLEND;
+		}
+		else if( minuteHand == 4 ) {
+			DEBUG_PRINT("DEMO | rainbow stripe with blend");
+			currentPalette = RainbowStripeColors_p;
+			currentBlending = BLEND;
+		}
+
+		if( random(0,10) < 4 ) {
+			_playingSong = true;
+			//	choose a song (theme or variation A)
+			_songIndex = (random(100) < 50)?0:1;
 		}
 		else {
-			return n.note;
+			_playingSong = false;
 		}
 	}
 }
 
-void DemoModeManager::changeDemoModePeriodically() {
-
+void DemoModeManager::fillLEDsFromPaletteColors(CRGB leds[],uint16_t colorIndex) {
+	uint8_t bright = 0xFF;
+	for(uint16_t i = 0; i < NUM_LEDS; i++ ) {
+		leds[i] = ColorFromPalette(currentPalette,colorIndex,bright,currentBlending);
+		colorIndex += 3;
+	}
 }
--- DemoModeManager.h
+++ DemoModeManager.h
@@ -19,21 +19,37 @@ typedef enum {
 	DemoModeTwinkleE
 } demo_mode_t;
 
+typedef enum {
+	DemoModeStateStopped,
+	DemoModeStatePaused,
+	DemoModeStateOn
+} demo_mode_state_t;
+
 typedef struct piano_roll_note_t {
 	uint8_t note;
 	uint32_t dur;		//	milliseconds
 } piano_roll_note_t;
 
+/**	Manages the demo mode
+ *
+ *	In the demo mode, the chair shows a shifting color palette,
+ *	occasionally interrupted by a "song".
+ *	The demo mode is started and stopped by the demo switch. It can also
+ *	be paused by some action on the touch sensors. When the touch sensors time out
+ *  The demo mode can be resumed.
+ */
 class DemoModeManager {
 public:
 	DemoModeManager(int ledPin);
-	uint8_t update();
+	uint8_t update(CRGB leds[]);
 	void start();
 	void stop();
 	bool state();
 	void override();
 	bool overrideState();
 private:
+	static uint8_t _songIndex;
+	static uint16_t _startIndex;
 	static bool _override;
 	static uint16_t _noteIndex;
 	static bool _noteState;
@@ -45,6 +61,8 @@ private:
 	static CRGBPalette16 currentPalette;
 	static TBlendType currentBlending;
 	static void changeDemoModePeriodically();
+	static void fillLEDsFromPaletteColors(CRGB leds[],uint16_t colorIndex);
+	static piano_roll_note_t noteFromSongWithIndex(uint8_t index,uint16_t aNoteIndex);
 };
 
 #endif
